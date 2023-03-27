4863eb9f611f65120cae7aaacc87842da20259b3
Kostya <kosg@mail.ru>
2023-03-28 00:01:06 +0300

    add

---
+++ home task 41/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int PositiveNumber(int[] array)
+{
+  int positive = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+      positive += 1;
+  }
+  return positive;
+}
+
+Console.Clear();
+Console.WriteLine("enter the numbers:");
+int [] array = Console.ReadLine().Split(", ").Select(x => int.Parse(x)).ToArray();
+Console.WriteLine($"[{string.Join(", ",array)}]");
+Console.WriteLine(PositiveNumber(array));
---
+++ home task 43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 ->  (-0,5; -0,5)
+
+void IntersectionLine(double[] array)
+{
+  double[] coord = new double[2];
+  coord[0] = Math.Round((array[2] - array[0]) / (array[1] - array[3]), 2);
+  coord[1] = Math.Round(array[1] * coord[0] + array[0], 2);
+  Console.WriteLine($"[{string.Join(", ",coord)}]");
+}
+Console.Clear();
+Console.WriteLine("enter the coefficient b1, k1, b2, k2:");
+double[] array = Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+Console.Write("coordinates: ");
+IntersectionLine(array);
